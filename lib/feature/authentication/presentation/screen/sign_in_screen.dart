74e1aeb7dc3fb9010f171ef663a705b189460f1d
vuong2001 <nguyenvanvuong301001@gmail.com>
2025-02-04 16:34:40 +0700

    update BlocListener to BlocBuilder

--- lib/feature/authentication/presentation/screen/sign_in_screen.dart
+++ lib/feature/authentication/presentation/screen/sign_in_screen.dart
@@ -13,6 +13,7 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
@@ -20,54 +21,55 @@ class SignInScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: BlocProvider(
-            create: (_) => AuthenticationCubit(
-                signIn: context.read<SignInUseCase>(),
-                signUp: context.read<SignUpUseCase>(),
-            ),
-          child: BlocListener<AuthenticationCubit, AuthenticationState>(
-              listener: (context, state){
-                if(state is Authenticated){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Login Success'))
-                  );
-                }
-                else if(state is AuthenticationError){
+          create: (_) => AuthenticationCubit(
+            signIn: context.read<SignInUseCase>(),
+            signUp: context.read<SignUpUseCase>(),
+          ),
+          child: BlocBuilder<AuthenticationCubit, AuthenticationState>(
+            builder: (context, state) {
+              if (state is Authenticated) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  context.go('/home');
+                });
+                return Container();
+              } else if (state is AuthenticationError) {
+                WidgetsBinding.instance.addPostFrameCallback((_){
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text(state.message))
                   );
-                }
-              },
-            child: Column(
-              children: [
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: 'email'),
-                ),
-                TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(hintText: 'password'),
-                  obscureText: true,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final email = emailController.text;
-                    final password = passwordController.text;
-                    print("Đang thực hiện đăng nhập với email: $email"); // Thêm print để kiểm tra
-                    await context.read<AuthenticationCubit>().signInUser(email, password);
-                    context.go('/home');
-                  },
-                  child: Text('Login'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final email = emailController.text;
-                    final password = passwordController.text;
-                    context.read<AuthenticationCubit>().signUpUser(email, password);
-                  },
-                  child: Text('Register'),
-                ),
-              ],
-            ),
+                });
+              }
+
+              return Column(
+                children: [
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: 'email'),
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(hintText: 'password'),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final email = emailController.text;
+                      final password = passwordController.text;
+                      context.read<AuthenticationCubit>().signInUser(email, password);
+                    },
+                    child: Text('Login'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      final email = emailController.text;
+                      final password = passwordController.text;
+                      context.read<AuthenticationCubit>().signUpUser(email, password);
+                    },
+                    child: Text('Register'),
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
