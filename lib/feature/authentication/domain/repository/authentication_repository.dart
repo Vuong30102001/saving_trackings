869db9b100ab35439d85bf87f8614a97eb234c7e
vuong2001 <nguyenvanvuong301001@gmail.com>
2025-02-04 10:36:03 +0700

    repository in domain layer implemention with 2 method signIn & signOut

---
+++ lib/feature/authentication/domain/repository/authentication_repository.dart
@@ -0,0 +1,6 @@
+import 'package:saving_trackings_flutter/feature/authentication/domain/entities/user_entity.dart';
+
+abstract class AuthenticationRepository {
+  Future<UserEntity> signInWithEmailPassword(String email, String password);
+  Future<UserEntity> signUpWithEmailPassword(String email, String password);
+}
